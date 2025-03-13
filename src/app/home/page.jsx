cb89f063464d41046ea70ffbd1f16efa71cfbdb1
Kamila-Irshad <kamilairshad366@gmail.com>
2025-03-13 22:00:37 +0500

    project start

---
+++ src/app/home/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+import headinglogo from  "../../../public/Images/Frame 34 (1).png"
+const Homepage =()=>{
+    return (
+        <>
+
+        <div>
+            <h1>logo</h1>
+            <Image>headinglogo</Image>
+            <button>Login/Registration</button>
+        </div>
+        </>
+    )
+}
+export default Homepage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Homepage from "@/app/home/page"
 
 export default function Home() {
   return (
     <>
+      <Homepage />
     </>
   );
 }
